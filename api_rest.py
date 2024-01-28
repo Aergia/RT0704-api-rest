a2b72d81624d2b65f83cd577baee686f58dc5382
Lucas Desinde <lucas.desinde@etudiant.univ-reims.fr>
2024-01-28 22:20:13 +0000

    Final

--- api_rest.py
+++ api_rest.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import os
+import requests
 
 path = os.path.dirname(__file__)
 app = Flask(__name__)
@@ -9,17 +10,20 @@ def hello():
     value = ["film1", "film2"]
     return render_template('index.html', films=value)
 
-@app.route('/register')
-def register():
-    return render_template('register.html')
-
 @app.route('/login')
 def login():
     return render_template('login.html')
 
 @app.route('/add', methods=['POST'])
 def add():
-    
+    print("mpm")
+
+@app.route('/print', methods=['POST'])
+def print():
+    name = str(request.get_data())[7:]
+    films = str(name[:int(len(name)-1)])
+
+    return render_template('index.html', films=films)
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
