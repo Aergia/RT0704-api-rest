d6edd6b06aec5f755386adec424846d82bb9fe1a
Lucas Desinde <lucas.desinde@etudiant.univ-reims.fr>
2024-01-28 10:23:57 +0000

    JInja Prototype + Base Templates

--- api_rest.py
+++ api_rest.py
@@ -1,15 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
 
+path = os.path.dirname(__file__)
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return '<h1>Videotheque</h1>'
+    value = ["film1", "film2"]
+    return render_template('index.html', films=value)
 
 @app.route('/register')
 def register():
-    return 'register'
+    return render_template('register.html')
 
 @app.route('/login')
 def login():
-    return 'login'
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='localhost', port=8000)
